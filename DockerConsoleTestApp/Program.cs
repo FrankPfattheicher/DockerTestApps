752976d3b02b4d918998dbf40de9effb659348fc
Sanjay Debnath <sanjayd@microsoft.com>
2017-12-28 03:39:20 +0530

    Add project files.

---
+++ DockerConsoleTestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DockerConsoleTestApp
+{
+    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var content = JsonConvert.DeserializeObject<Content>(File.ReadAllText("quotes.json"));
+
+            var r = new Random().Next(content.Quotes.Length);
+
+            Console.WriteLine(content.Quotes[r]);
+        }
+
+        private class Content
+        {
+            public string[] Quotes { get; set; }
+        }
+    }
+}
