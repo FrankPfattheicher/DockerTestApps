03e3af4b92058e62197dff9e3b6b6a73dd8224e9
Sanjay Debnath <sanjayd@microsoft.com>
2017-12-28 04:49:33 +0530

    Few small updated

--- DockerConsoleTestApp/Program.cs
+++ DockerConsoleTestApp/Program.cs
@@ -2,17 +2,20 @@
 {
     using System;
     using System.IO;
+    using System.Threading;
     using Newtonsoft.Json;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var content = JsonConvert.DeserializeObject<Content>(File.ReadAllText("quotes.json"));
-
-            var r = new Random().Next(content.Quotes.Length);
-
-            Console.WriteLine(content.Quotes[r]);
+            while (true)
+            {
+                Thread.Sleep(2000);
+                var content = JsonConvert.DeserializeObject<Content>(File.ReadAllText("quotes.json"));
+                var r = new Random().Next(content.Quotes.Length);
+                Console.WriteLine(content.Quotes[r]);
+            }
         }
 
         private class Content
